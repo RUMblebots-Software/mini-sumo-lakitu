5ded0b372cd00f58d0af794135b685c1c4eb7851
CdoxHype <julio.camacho3@upr.edu>
2025-04-08 18:56:06 -0400

    Image modf to one image

--- data/Data Modification/image_modification.py
+++ data/Data Modification/image_modification.py
@@ -2,6 +2,16 @@ import albumentations as A
 import cv2
 import numpy as np
 import os
+from matplotlib import pyplot as plt
+
+
+
+def visualize(image):
+    plt.figure(figsize=(10,10))
+    plt.axis("off")
+    plt.imshow(image)
+
+
 
 input_folders = ["../Data Acquisition/Flag.class","../Data Acquisition/MiniSumo.class","../Data Acquisition/NotMiniSumo.class"]
 output_folders = ["Data Modification/Flag.class","Data Modification/MiniSumo.class","Data Modification/NotMiniSumo.class"]
@@ -9,15 +19,20 @@ output_folders = ["Data Modification/Flag.class","Data Modification/MiniSumo.cla
 
 #Pipeline Definition 
 pipeline = A.Compose([
-    A.HorizontalFlip(p=0.5), # 50% chance to flip
-    A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
-    A.GaussianBlur(p=0.3), # 30% chance to blur
+    A.HorizontalFlip(p=0.99), # 50% chance to flip
+    #A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
+    #A.GaussianBlur(p=0.3), # 30% chance to blur
 ])
 
 
 for folder in input_folders:
     for file_name in os.listdir(folder):
-        continue 
+        img = cv2.imread(os.path.join(folder,file_name))
+
+        pipelined = pipeline(image=img)
+        trasnformed = pipelined["image"]
+        break
+visualize(trasnformed)
     
 
 
