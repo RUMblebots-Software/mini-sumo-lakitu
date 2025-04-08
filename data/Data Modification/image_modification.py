7d9060c5e05e7d4614ec2ab8e665ce255f4e773e
CdoxHype <julio.camacho3@upr.edu>
2025-04-08 19:30:04 -0400

    Image modification done, Next steps search for necessary Trasformations

--- data/Data Modification/image_modification.py
+++ data/Data Modification/image_modification.py
@@ -16,14 +16,15 @@ def visualize(image):
 input_folders = ["../Data Acquisition/Flag.class","../Data Acquisition/MiniSumo.class","../Data Acquisition/NotMiniSumo.class"]
 output_folders = ["Data Modification/Flag.Augmented","Data Modification/MiniSumo.Augmented","Data Modification/NotMiniSumo.Augmented"]
 
-
+#---------Look for necessary trasnformation--------------------
 #Pipeline Definition 
 pipeline = A.Compose([
-    A.HorizontalFlip(p=0.99), # 50% chance to flip
-    #A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
-    #A.GaussianBlur(p=0.3), # 30% chance to blur
+    A.HorizontalFlip(p=0.5), # 50% chance to flip
+    A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
+    A.GaussianBlur(p=0.3), # 30% chance to blur
 ])
 
+#------- Applie the pipeline to each photo in the three folders--------------
 
 for folder in input_folders:
     for file_name in os.listdir(folder):
@@ -31,8 +32,8 @@ for folder in input_folders:
 
         pipelined = pipeline(image=img)
         trasnformed = pipelined["image"]
-        break
-visualize(trasnformed)
+        
+
     
 
 
