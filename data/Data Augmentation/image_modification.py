62c346bc52fb9765fdf3d397b0d4d083126005eb
JeanSanchezFelix <jeanpiere.sanchez@upr.edu>
2025-04-09 09:23:47 -0400

    Changed the name of the modified data

---
+++ data/Data Augmentation/image_modification.py
@@ -0,0 +1,43 @@
+import albumentations as A
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+
+
+
+def visualize(image):
+    plt.figure(figsize=(10,10))
+    plt.axis("off")
+    plt.imshow(image)
+
+
+
+input_folders = ["../Data Acquisition/Flag.class","../Data Acquisition/MiniSumo.class","../Data Acquisition/NotMiniSumo.class"]
+output_folders = ["Data Augmentation/Flag.Augmented","Data Augmentation/MiniSumo.Augmented","Data Augmentation/NotMiniSumo.Augmented"]
+
+
+#Pipeline Definition 
+pipeline = A.Compose([
+    A.HorizontalFlip(p=0.99), # 50% chance to flip
+    #A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
+    #A.GaussianBlur(p=0.3), # 30% chance to blur
+])
+
+
+for folder in input_folders:
+    for file_name in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,file_name))
+
+        pipelined = pipeline(image=img)
+        trasnformed = pipelined["image"]
+        break
+visualize(trasnformed)
+    
+
+
+
+
+
+
+
