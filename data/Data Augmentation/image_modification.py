771ea077423a6f6d4d3e41796d003ca36a8b7e24
marymarmaldonado <marymar.maldonado@upr.edu>
2025-04-22 19:08:23 -0400

    added transformations

--- data/Data Augmentation/image_modification.py
+++ data/Data Augmentation/image_modification.py
@@ -19,11 +19,31 @@ names = ["Flag","MiniSumo","NotMiniSumo"]
 
 #---------Look for necessary trasnformation--------------------
 #Pipeline Definition 
-pipeline = A.Compose([
-    A.ToSepia(p=0.99), # 50% chance to flip
-    A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
-    A.GaussianBlur(p=0.3), # 30% chance to blur
-])
+
+prob_pipeline1 = 0.90
+prob_pipeline2 = 0.80
+
+
+pipeline = A.OneOf([
+    
+    A.Compose([
+        A.RandomShadow(p=0.90), #Simulates shadows for the image by reducing the brightness of the image in shadow regions.
+        A.RandomBrightnessContrast(p=0.90), # 80% chance to adjust brightness/contrast
+        A.RandomRotate90(p=0.90), #Randomly rotate the input by 90 degrees zero or more times.
+        A.Illumination(p=0.90), #Apply various illumination effects to the image.
+   
+        A.OneOf([
+            A.MotionBlur(p=0.70), #Apply motion blur to the input image using a directional kernel.
+            A.ZoomBlur (p=0.90), #Apply zoom blur transform.    
+        ], p=0.75)  
+    ], p=prob_pipeline1),                  # The entire pipeline has a 100% chance to be applied
+
+    A.Compose([
+        A.Downscale(scale_min=0.25, scale_max=0.5, p=0.80), #Decrease image quality by downscaling and upscaling back.
+        A.RandomGravel(p=0.50), #Adds gravel-like artifacts to the input image.
+        A.Resize(height=256, width=256, p=0.30), #Resize the input to the given height and width
+    ], p=prob_pipeline2,)
+], p = 0.50)
 
 #------- Applie the pipeline to each photo in the three folders then save it --------------
 output_folders_index = -1
