3e7b191df3c57b05294af751b9d880522d905901
CdoxHype <julio.camacho3@upr.edu>
2025-04-22 18:54:37 -0400

    Automatic Folder Images Saving

--- data/Data Augmentation/image_modification.py
+++ data/Data Augmentation/image_modification.py
@@ -13,26 +13,39 @@ def visualize(image):
 
 
 
-input_folders = ["../Data Acquisition/Flag.class","../Data Acquisition/MiniSumo.class","../Data Acquisition/NotMiniSumo.class"]
-output_folders = ["Data Augmentation/Flag.Augmented","Data Augmentation/MiniSumo.Augmented","Data Augmentation/NotMiniSumo.Augmented"]
+input_folders = ["../Data Acquisition/Flag.class", "../Data Acquisition/MiniSumo.class", "../Data Acquisition/NotMiniSumo.class"]
+output_folders = ["Flag.Augmented","MiniSumo.Augmented","NotMiniSumo.Augmented"]
+names = ["Flag","MiniSumo","NotMiniSumo"]
 
 #---------Look for necessary trasnformation--------------------
 #Pipeline Definition 
 pipeline = A.Compose([
-    A.HorizontalFlip(p=0.5), # 50% chance to flip
+    A.ToSepia(p=0.99), # 50% chance to flip
     A.RandomBrightnessContrast(p=0.8), # 80% chance to adjust brightness/contrast
     A.GaussianBlur(p=0.3), # 30% chance to blur
 ])
 
-#------- Applie the pipeline to each photo in the three folders--------------
-
+#------- Applie the pipeline to each photo in the three folders then save it --------------
+output_folders_index = -1
+output_file_name = 00
 for folder in input_folders:
+    output_folders_index += 1
     for file_name in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,file_name))
 
+        #If failed to load image this will skip the proccess
+        if img is None:
+            print(f"Failed to load image {img} from {folder}")
+            break
+
         pipelined = pipeline(image=img)
         trasnformed = pipelined["image"]
-        
+
+
+        save_to_folder = os.path.join(output_folders[output_folders_index],f"Augmented_{names[output_folders_index]}_{output_file_name}.jpg")
+        cv2.imwrite(save_to_folder,trasnformed)
+        output_file_name += 1
+    output_file_name = 00  
 
     
 
