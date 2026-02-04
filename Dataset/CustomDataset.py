8ec463e3d091bf991242f7c7cf3753baebeab6c6
yadriel.rivera <yadriel.rivera@upr.edu>
2026-02-03 21:08:53 -0400

    added methods to be used

--- Dataset/CustomDataset.py
+++ Dataset/CustomDataset.py
@@ -1,17 +1,41 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
+from torchvision.datasets.folder import default_loader
 from PIL import Image
 import json
 import os
 
-class CustomImageBoundingBoxDataset(Dataset):
-    def __init__(self, img_dir, json_dir, transform=None):
-        self.img_dir = img_dir
-        self.json_dir = json_dir
-        self.transform = transform
-        # Get list of all image filenames
-        self.img_files = [f for f in os.listdir(img_dir) if f.endswith('.png') or f.endswith('.jpg')]
-        # Assuming JSON filenames match image filenames (e.g., image.jpg -> image.json)
+class BoundingBoxImageFolder(ImageFolder):
+    def __init__(
+            self, 
+            img_dir, 
+            json_dir, 
+            transform=None, 
+            target_transform=None, 
+            loader=default_loader, 
+            is_valid_file = None
+        ):
+            super().__init__(
+                root=img_dir,
+                transform=transform,
+                target_transform=target_transform,
+                loader=loader,
+                is_valid_file=is_valid_file
+            )
+            self.img_dir = img_dir
+            self.json_dir = json_dir
+            
+            # Get list of all image filenames
+            self.img_files = [f for f in os.listdir(img_dir) if f.endswith('.png') or f.endswith('.jpg')]
+            # Assuming JSON filenames match image filenames (e.g., image.jpg -> image.json)
+        
+    
+    def find_bounding_boxes(self, json_dir):
+        return
+
+    def make_dataset(self, ):
+        return
 
     def __len__(self):
         return len(self.img_files)
@@ -43,10 +67,18 @@ class CustomImageBoundingBoxDataset(Dataset):
         # Add other relevant information like labels, image_id, etc.
         # target["labels"] = torch.as_tensor([obj['label'] for obj in annotation['objects']], dtype=torch.int64)
 
-        if self.transform:
-            # Note: The transforms for object detection usually require applying
-            # the same transformation to both the image and the bounding boxes.
-            # Torchvision's new transforms API handles this with tv_tensors.
-            image, target = self.transform(image, target)
+
 
         return image, target
+
+
+
+# class BondingBoxDataset(Dataset):
+#     def __init__(self):
+#         self.dataset = BoundingBoxImageFolder(img_dir, json_dir, transform)
+
+#         if self.transform:
+#         # Note: The transforms for object detection usually require applying
+#         # the same transformation to both the image and the bounding boxes.
+#         # Torchvision's new transforms API handles this with tv_tensors.
+#         image, target = self.transform(image, target)
